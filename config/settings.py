a1f9658ca813609e058291fcc51f6d23a1533283
Oleksandr Kaminskyi (EXT-Nokia) <oleksandr.kaminskyi.ext@nokia.com>
2026-03-25 18:19:07 +0200

    hide sensitive data

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,9 @@
-BASE_URL = "http://eaapi.somee.com/"   
-AUTH_USER = "admin"
-AUTH_PASSWORD = "password"
+import os
+
+
+BASE_URL = os.getenv("BASE_URL")
+AUTH_USER = os.getenv("AUTH_USER_NAME")
+AUTH_PASSWORD = os.getenv("AUTH_PASSWORD")
 
 # If token reused across tests:
 TOKEN_CACHE = ".token_cache"
