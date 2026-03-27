19f0e522d2c37095a1b0570ae584c9082bd30156
Oleksandr Kaminskyi (EXT-Nokia) <oleksandr.kaminskyi.ext@nokia.com>
2026-03-27 12:23:03 +0200

    debug ci env vars

--- config/settings.py
+++ config/settings.py
@@ -5,5 +5,9 @@ BASE_URL = os.getenv("BASE_URL", "http://eaapi.somee.com/")
 AUTH_USER = os.getenv("AUTH_USER_NAME")
 AUTH_PASSWORD = os.getenv("AUTH_PASSWORD")
 
+print(f"Using base URL: {BASE_URL}")
+print(f"Using auth user: {AUTH_USER}")
+print(f"Using auth password: {'*' * len(AUTH_PASSWORD) if AUTH_PASSWORD else None}")
+
 # If token reused across tests:
 TOKEN_CACHE = ".token_cache"
